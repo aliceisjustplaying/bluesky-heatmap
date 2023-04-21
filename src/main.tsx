185d0142dadf40041022b024b24a3a41ff84b4ea
alice <aliceisjustplaying@gmail.com>
2023-04-21 17:39:06 +0100

    fork me on github

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import 'react-tooltip/dist/react-tooltip.css';
 import './react-calendar-heatmap.css';
 import './styles.css';
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 import { App } from './App';
 
 const container = document.getElementById('root');
