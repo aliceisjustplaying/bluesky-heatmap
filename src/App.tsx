bce0a4eb9cf3b2f173447f0d305873600ef520ed
alice <aliceisjustplaying@gmail.com>
2023-04-22 14:05:04 +0100

    refactor (thank you brundolf)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useMemo, useCallback, FormEvent, ChangeEvent } from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import { Tooltip } from 'react-tooltip';
 import * as bsky from '@atproto/api';
@@ -6,114 +6,109 @@ const { BskyAgent } = bsky;
 import type { AtpSessionEvent, AtpSessionData } from '@atproto/api';
 import { getData } from './atproto.tsx';
 
+type PostsData = Awaited<ReturnType<typeof getData>>;
+
 export const App = () => {
-  const [posts, setPosts] = useState<any>([]);
-  const [max, setMax] = useState<number>(0);
-  const [createdAt, setCreatedAt] = useState<Date>();
-  const [actor, setActor] = useState<string>('');
-  const [updated, setUpdated] = useState<string>(actor);
+  const [data, setData] = useState<PostsData | undefined>();
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [loginPressed, setLoginPressed] = useState<boolean>(false);
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [heatmapSubject, setHeatmapSubject] = useState<string>('');
   const [session, setSession] = useState<AtpSessionData>();
-  const [agent, setAgent] = useState<bsky.BskyAgent>();
 
-  useMemo(() => {
-    const agent = new BskyAgent({
-      service: 'https://bsky.social',
-      persistSession: (_evt: AtpSessionEvent, sess?: AtpSessionData) => {
-        setSession(sess!);
-      },
+  const posts = data?.data ?? [];
+  const max = data?.max ?? 0;
+  const createdAt = new Date(data?.createdAt);
+
+  const agent = useMemo(
+    () =>
+      new BskyAgent({
+        service: 'https://bsky.social',
+        persistSession: (_evt: AtpSessionEvent, sess?: AtpSessionData) => {
+          console.log('setSession', sess);
+          if (sess != null) {
+            setSession(sess!);
+          }
+        },
+      }),
+    [],
+  );
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const loadPosts = useCallback(async () => {
+    setIsLoading(true);
+    console.log('loadPosts');
+    try {
+      const data = await getData(agent!, session!, heatmapSubject);
+      setData(data);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [agent, session, heatmapSubject]);
+
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const login = useCallback(async () => {
+    await agent!.login({
+      identifier: username,
+      password: password,
     });
+    setLoggedIn(true);
+  }, [username, password, agent]);
 
-    setAgent(agent);
+  const handleLoginSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      await login();
+    },
+    [login],
+  );
+
+  const handleUsernameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+    setHeatmapSubject(e.target.value);
   }, []);
 
-  useEffect(() => {
-    let ignore = false;
-    setPosts([]);
+  const handlePasswordChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, []);
 
-    if (loggedIn) {
-      setIsLoading(true);
-      getData(agent!, session!, updated).then((data) => {
-        if (!ignore) {
-          setPosts(data.data);
-          setMax(data.max);
-          setCreatedAt(new Date(data.createdAt));
-          setIsLoading(false);
-        }
-      });
-    }
-
-    return () => {
-      ignore = true;
-    };
-  }, [agent, session, updated, loggedIn]);
-
-  useEffect(() => {
-    let ignore = false;
-    if (loginPressed && !ignore) {
-      agent!
-        .login({
-          identifier: username,
-          password: password,
-        })
-        .then(() => {
-          setLoggedIn(true);
-        });
-    }
-    return () => {
-      ignore = true;
-    };
-  }, [agent, username, password, loginPressed]);
+  const handleHeatmapSubjectChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setHeatmapSubject(e.target.value);
+  }, []);
 
   return (
     <div>
       <h1>Bluesky Posts Heatmap Generator</h1>
-      {loggedIn === false ? (
+      {!loggedIn && (
         <>
           <div id="loginMessage">Please log in</div>
           <br />
         </>
-      ) : null}
-      <div id="login">
+      )}
+      <form id="login" onSubmit={handleLoginSubmit}>
         Username:&nbsp;
-        <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} value={username} />
+        <input type="text" placeholder="username" onChange={handleUsernameChange} value={username} />
         <br />
         Password:&nbsp;
-        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-        <input
-          type="button"
-          value="login"
-          onClick={() => {
-            setLoginPressed(true);
-            setActor(username);
-          }}
-          disabled={loggedIn}
-        />
+        <input type="password" placeholder="password" onChange={handlePasswordChange} value={password} />
+        <input type="submit" value="login" disabled={loggedIn} />
         <br />
         <br />
-      </div>
+      </form>
       <div id="actor">
         🦋&nbsp;
-        <input type="text" placeholder="Bluesky username" onChange={(e) => setActor(e.target.value)} value={actor} />
         <input
-          type="button"
-          value="Get heatmap"
-          onClick={() => {
-            setMax(0);
-            setUpdated(actor);
-          }}
-          disabled={!loggedIn || isLoading}
+          type="text"
+          placeholder="Bluesky username"
+          onChange={handleHeatmapSubjectChange}
+          value={heatmapSubject}
         />
+        <input type="button" value="Get heatmap" onClick={loadPosts} disabled={!loggedIn || isLoading} />
       </div>
       <div>
         <br />
       </div>
       {isLoading ? <div>Loading...</div> : null}
-      {posts.length === 0 ? null : (
+      {posts.length === 0 || isLoading ? null : (
         <>
           <CalendarHeatmap
             startDate={createdAt}
